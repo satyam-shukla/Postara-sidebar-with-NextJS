9b82c36780b4ef361710f8feca1c36f98ca289d7
satyam-shukla <satyam20@navgurukul.org>
2021-11-28 19:23:29 +0530

    done

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+import Home from './side_var'
+
+export default function post() {
+  return (
+    <Home>
+
+    </Home>
+
+  )
+}
